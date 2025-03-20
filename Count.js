0a68f056b0630f5a87f8b348fb136c62543b8a58
Om Chokse <omchokse30@gmail.com>
2025-03-20 19:03:39 +0530

    Count by city or state

---
+++ Count.js
@@ -0,0 +1,17 @@
+let addressBook = require('./AddContacts');
+function countByCityOrState(addressBook, groupBy) {
+    let count = addressBook.reduce((acc, contact) => {
+        let key = groupBy === "city" ? contact.city : contact.state;
+        acc[key] = (acc[key] || 0) + 1;
+        return acc;
+    }, {});
+
+    return count;
+}
+
+// Example Usage
+let countByCity = countByCityOrState(addressBook, "city");
+console.log("Count by City:", countByCity);
+
+let countByState = countByCityOrState(addressBook, "state");
+console.log("Count by State:", countByState);
